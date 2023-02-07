5accf0094d98e54c7add6b6b311e0b39cb72a906
Kootyra <110038102+Kootyra@users.noreply.github.com>
2023-02-07 21:28:02 +0100

    for_cloud

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
@@ -27,3 +28,8 @@ urlpatterns = [
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.nomber500'
 handler403 = 'core.views.csrf_failure'
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
